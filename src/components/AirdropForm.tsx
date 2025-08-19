dd27ebb30b483062c794c5193d30b41a847a9f6a
Huss Martinez <husse.dev@gmail.com>
2025-08-19 21:45:24 +0700

    feat: implemented AirdropForm component

---
+++ src/components/AirdropForm.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import InputField from "./ui/InputField";
+
+export default function AirdropForm() {
+  const [tokenAddress, setTokenAddress] = useState("");
+  const [recipients, setRecipients] = useState("");
+  const [amounts, setAmounts] = useState("");
+
+  async function handleSubmit() {
+    console.log("tokenAddress", tokenAddress);
+    console.log("recipients", recipients);
+    console.log("amounts", amounts);
+    // Approve our tsender contract to send our tokens
+    // Call the airdrop function on the tsender contract
+    // Wait for the transaction to be mined
+  }
+  return (
+    <div>
+      <InputField
+        label="Token Address"
+        placeholder="0x"
+        value={tokenAddress}
+        onChange={e => setTokenAddress(e.target.value)}
+      />
+      <InputField
+        large
+        label="Recipients"
+        placeholder="0x1111....aaAA,0x2222....bbBB,0x3333....ccCC"
+        value={recipients}
+        onChange={e => setRecipients(e.target.value)}
+      />
+      <InputField
+        large
+        label="Amounts"
+        placeholder="100,200,300"
+        value={amounts}
+        onChange={e => setAmounts(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Send tokens</button>
+    </div>
+  );
+}
