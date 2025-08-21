8e1bc4d4a7f99d8b96388077df1fd7facf2241f0
Huss Martinez <husse.dev@gmail.com>
2025-08-21 21:53:22 +0700

    feat: render a message instead of the form if not connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import AirdropForm from "@/components/AirdropForm";
+import { useAccount } from "wagmi";
 
 export default function Home() {
-  return (
+  const { isConnected } = useAccount();
+  return isConnected ? (
     <div>
       <AirdropForm />
+    </div>
+  ) : (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center text-gray-600 dark:text-gray-400">
+        Please connect your wallet to use the airdrop feature.
+      </div>
     </div>
   );
 }
