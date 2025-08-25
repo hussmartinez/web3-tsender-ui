63ce6a6283f8e029ee27ac6e17b82f3218a5da6a
hussmartinez <husse.dev@gmail.com>
2025-08-26 02:56:40 +0700

    fix: chains for production

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, zksync } from "wagmi/chains";
+import { anvil, mainnet, sepolia, zksync, zksyncSepoliaTestnet } from "wagmi/chains";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+const isProduction = process.env.NODE_ENV === "production";
 
 export default getDefaultConfig({
   appName: "TSender",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!, // WalletConnect
-  chains: [anvil, zksync],
+  chains: isProduction ? [mainnet, zksync] : [mainnet, zksync, anvil, sepolia, zksyncSepoliaTestnet],
   ssr: false,
 });
